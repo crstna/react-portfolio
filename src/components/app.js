6f7c8454cc0b75955fc28c9420ac7d211258c1c2
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-11 09:51:06 -0600

    connecting with portfolio-container

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
+
+import PortfolioContainer from './portfolio-container'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>cristina wood portfolio</h1>
+        <PortfolioContainer />
         <div>
           {moment().format('L')}
         </div>
