aad42a4eb8e8312f3a5f6ab3f560f9b8823e5364
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-15 11:34:37 -0600

    missing = sign

--- src/components/app.js
+++ src/components/app.js
@@ -25,7 +25,7 @@ export default class App extends Component {
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path "/blog" component={Blog} />
+              <Route path="/blog" component={Blog} />
             </switch>
           </div>
         </Router>
