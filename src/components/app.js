4faf0d06181df64018370b2e20298b43fbad3494
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-15 09:24:30 -0600

    new js stuff

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 import PortfolioContainer from './portfolio/portfolio-container'
+import Navigationcontainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>cristina wood portfolio</h1>
         <div>{moment().format('L')}</div>
         <PortfolioContainer />
