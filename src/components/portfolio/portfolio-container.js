52256c1622cfc9a1573d3641238d49053997ba72
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-16 11:43:08 -0600

    remove debugger

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -35,7 +35,6 @@ export default class PortfolioContainer extends Component {
       }
     portfolioItems() {
         return this.state.data.map(item => {
-            debugger;
             return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
         });
     }
