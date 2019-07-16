fd7b0885d1bfe338d9e1d62996e944bfdaf716ee
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-16 10:50:33 -0600

    wowza

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -25,7 +25,6 @@ export default class PortfolioContainer extends Component {
         axios
         .get("https://cristina.devcamp.space/portfolio/portfolio_items")
         .then(response => {
-          console.log("response data", response);
           this.setState({
               data: response.data.portfolio_items
           })
@@ -36,7 +35,7 @@ export default class PortfolioContainer extends Component {
       }
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
+            return <PortfolioItem title={item.name} url={item.url} slug={item.id} />;
         });
     }
     componentDidMount() {
