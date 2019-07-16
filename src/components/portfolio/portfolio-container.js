a9ec8940cd43e74e35c1543ae4d009dedbf83a08
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-16 11:32:01 -0600

    name

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -35,7 +35,7 @@ export default class PortfolioContainer extends Component {
       }
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.id} url={item.url} slug={item.id} />;
+            return <PortfolioItem title={item.name} url={item.url} slug={item.id} />;
         });
     }
     componentDidMount() {
