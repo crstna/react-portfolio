4fdcfe95b529e179c5bafa5276342e6c17576591
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-17 11:47:25 -0600

    fhteruh

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import {
   BrowserRouter as Router,
   Switch,
@@ -24,8 +23,6 @@ export default class App extends Component {
       <div className='app'>
         <Router>
           <div>
-            <h1>cristina wood portfolio</h1>
-            <div>{moment().format('L')}</div>
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home} />
