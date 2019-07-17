0b50fa4c8d680bd7714ca94a6be7252b86612190
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-17 08:29:09 -0600

    added notes and remove debugger

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -35,12 +35,11 @@ export default class PortfolioContainer extends Component {
       }
     portfolioItems() {
         // Data that we'll need:
-        // - background image
+        // - background image: "thumb_image_url"
         // - logo
-        // - description
-        // - id
+        // - description: description
+        // - id: id
         return this.state.data.map(item => {
-            debugger;
             return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
         });
     }
