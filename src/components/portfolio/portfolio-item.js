2da13f36f9132110e2d94c1c933783eaf39e3d49
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-19 09:24:06 -0600

    hgd

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function(props) {
-    const { id, description, thumb_image_url, logo_url} = props.item;
-    return (
-        <div className="portfolio-item-wrapper">
-            <div
-                className="portfolio-img-background"
-                style={{
-                    backgroundImage: "url(" + thumb_image_url + ")"
-                }}
-            />
+export default class PortoflioItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
+    handleMouseenter() {
+        this.setState({ portfolioItemClass: "image-blur" });
+    }
+    render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item;
+        return ( 
+            <div 
+            className="portfolio-item-wrapper">
+            onMouseEnter={() => this.handleMouseenter()}
+            onMouseLeave={() => this.handleMouseLeave()}
+            >
+                <div
+                    className={
+                    "portfolio-img-background " + this.state.portfolioItemClass
+                    }
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />
             
-            <div className="img-text-wrapper">
-                <div className="logo-wrapper">
-                    <img src={logo_url} />
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+                    <div className="subtitle">{description}</div>
                 </div>
-                <div className="subtitle">{description}</div>
             </div>
-        </div>
-    );
+        );
+    }
 }
