9a4876c06f3364ede7ded75c0d25a53b8a9a1138
CRISTINA WOOD <woodcr@wsd.ad>
2019-07-19 08:45:08 -0600

    rewt

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -9,10 +9,19 @@ export default function(props) {
     const { id, description, thumb_image_url, logo} = props.item;
     return (
         <div className="portfolio-item-wrapper">
-            <img src={thumb_image_url} />
-            <img src={logo} />
-            <div>{description}</div>
-            <Link to={`/portfolio/${id}`}>link</Link>
+            <div
+            className="portfolio-img-background"
+            style={{
+                backgroundImage: "url(" + thumb_image_url + ")"
+            }}
+            />
+            
+            <div className="img-text-wrapper">
+                <div className="logo-wrapper">
+                    <img src={logo} />
+                </div>
+                <div className="subtitle">{description}</div>
+            </div>
         </div>
     );
 }
